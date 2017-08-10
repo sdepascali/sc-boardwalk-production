ab08a4b71792bc8a8cdcefbd20303d977358db9c
paleov <admin@swarmcity.io>
2017-08-09 19:02:39 -0700

    remove helmet, remove cluster, package.json copy had issues so move copy to dockerfile

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-var prpl = require('prpl-server');
-var express = require('express');
-var helmet = require('helmet');
-var app = express();
-var server = require('http').createServer(app);
+const prpl = require('prpl-server');
+const express = require('express');
+const config = require('./build/polymer.json');
 
-app.use(helmet());
+const app = express();
 
-app.get('/*', prpl.makeHandler('./build/', {
-    builds: [
-        {name: 'es5-bundled', browserCapabilities: ['es2015'], basePath: true},
-        {name: 'es6-unbundled', browserCapabilities: ['push'], basePath: true},
-        {name: 'es6-bundled', browserCapabilities: [], basePath: true},
-    ],
-}));
+app.get('/*', prpl.makeHandler('./build/', config));
 
-const PORT = 8080;
 const HOST = '0.0.0.0';
+app.listen(8080, HOST);
 
 
-server.listen(PORT, HOST);
+// var prpl = require('prpl-server');
+// var express = require('express');
+// var helmet = require('helmet');
+// var app = express();
+// var server = require('http').createServer(app);
+//
+// app.use(helmet());
+//
+// app.get('/*', prpl.makeHandler('./build/', {
+//     builds: [
+//         {name: 'es5-bundled', browserCapabilities: ['es2015'], basePath: true},
+//         {name: 'es6-unbundled', browserCapabilities: ['push'], basePath: true},
+//         {name: 'es6-bundled', browserCapabilities: [], basePath: true},
+//     ],
+// }));
+//
+// const PORT = 8080;
+// const HOST = '0.0.0.0';
+//
+//
+// server.listen(PORT, HOST);
