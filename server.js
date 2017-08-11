9693ce0399bb675b633659f4566f778c308e03ce
paleov <admin@swarmcity.io>
2017-08-10 19:37:11 -0700

    looking for polymer build bug, temp remove build steps for contracts

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
-const prpl = require('prpl-server');
-const express = require('express');
-const config = require('./build/polymer.json');
+var prpl = require('prpl-server');
+var express = require('express');
+var helmet = require('helmet');
+var app = express();
+var server = require('http').createServer(app);
 
-const app = express();
+app.use(helmet());
 
-app.get('/*', prpl.makeHandler('./build/', config));
+app.get('/*', prpl.makeHandler('./build/', {
+    builds: [
+        {name: 'es5-bundled', browserCapabilities: ['es2015']},
+        {name: 'es6-unbundled', browserCapabilities: ['push']},
+        {name: 'es6-bundled', browserCapabilities: []},
+    ],
+}));
 
+const PORT = 8080;
 const HOST = '0.0.0.0';
-app.listen(8080, HOST);
 
-
-// var prpl = require('prpl-server');
-// var express = require('express');
-// var helmet = require('helmet');
-// var app = express();
-// var server = require('http').createServer(app);
-//
-// app.use(helmet());
-//
-// app.get('/*', prpl.makeHandler('./build/', {
-//     builds: [
-//         {name: 'es5-bundled', browserCapabilities: ['es2015'], basePath: true},
-//         {name: 'es6-unbundled', browserCapabilities: ['push'], basePath: true},
-//         {name: 'es6-bundled', browserCapabilities: [], basePath: true},
-//     ],
-// }));
-//
-// const PORT = 8080;
-// const HOST = '0.0.0.0';
-//
-//
-// server.listen(PORT, HOST);
+server.listen(PORT, HOST);
